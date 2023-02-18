0fbcc52184d8257f10e6ad43dcd478dfe9805754
evilditch <ronja@pahaoja.fi>
2023-02-18 17:05:35 +0200

    Juttujen listaelementin pohja

---
+++ hacker-news/src/App.js
@@ -0,0 +1,13 @@
+import StoriesList from './components/StoriesList'
+
+
+const App = () => {
+  return (
+    <>
+      <h1>The most read stories of Hacker News</h1>
+    <StoriesList />
+    </>
+  )
+}
+
+export default App
---
+++ hacker-news/src/components/StoriesList.js
@@ -0,0 +1,11 @@
+
+const StoriesList = () => {
+  return (
+    <ul>
+      <li>Eka juttu</li>
+      <li>Toka juttu</li>
+    </ul>
+  )
+}
+
+export default StoriesList
